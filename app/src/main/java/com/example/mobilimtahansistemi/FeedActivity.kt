ee174ac80c853a351085105757c9d4202f722491
Huseyn <90988414+HuS3ynMRZ@users.noreply.github.com>
2022-05-13 23:41:08 +0400

    Initial commit

---
+++ app/src/main/java/com/example/mobilimtahansistemi/FeedActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mobilimtahansistemi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mobilimtahansistemi.databinding.ActivityFeedBinding
+
+class FeedActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFeedBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFeedBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.recyclerView
+    }
+}
